4ffabfae2957349ab25aa853e7af6cb5f7f4e0da
agung <m.agungmahardika12@gmail.com>
2025-03-26 19:09:02 +0700

    add sumbar geom to public

--- app/Views/landing_page.php
+++ app/Views/landing_page.php
@@ -38,6 +38,7 @@
 
     <!-- Google Maps API and Custom JS -->
     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB8B04MTIk7abJDVESr6SUF6f3Hgt1DPAY&libraries=drawing"></script>
+    <script src="<?= base_url('js/geom.js'); ?>"></script>
     <script src="<?= base_url('js/web.js'); ?>"></script>
 </head>
 
