e4d43e66bef695de02d01aeb1dd44836e4391604
agung <m.agungmahardika12@gmail.com>
2025-05-04 15:08:34 +0700

    remove phone column

--- app/Controllers/Api/User.php
+++ app/Controllers/Api/User.php
@@ -104,7 +104,6 @@ class User extends ResourceController
             'last_name' => $request['last_name'],
             'email' => $request['email'],
             'address' => $request['address'] ?? '', // Gunakan null coalescing untuk field yang opsional
-            'phone' => $request['phone'] ?? '',     // Gunakan null coalescing untuk field yang opsional
             'password' => $passwordHash, // Simpan hash, bukan password asli
             'avatar' => $request['avatar'] ?? '',    // Gunakan null coalescing untuk field yang opsional
             'role_id' => $request['role_id'],
