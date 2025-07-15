d9e11b2f25281fa886edb231086f847cab2f9233
ranggi <ranggiaureliyanto@gmail.com>
2025-07-15 20:21:17 +0600

    modal title color

--- app/Views/mobile/maps/rumah_gadang.php
+++ app/Views/mobile/maps/rumah_gadang.php
@@ -30,8 +30,8 @@
 <div class="modal fade text-left" id="routeAllModal" tabindex="-1" aria-labelledby="routeAllModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-dialog-scrollable modal-lg" role="document">
         <div class="modal-content">
-            <div class="modal-header bg-success text-white">
-                <h5 class="modal-title" id="routeAllModalLabel">Daftar Rute</h5>
+            <div class="modal-header bg-primary text-white">
+                <h5 class="modal-title text-white" id="routeAllModalLabel">Daftar Rute</h5>
                 <button type="button" class="close rounded-pill" data-bs-dismiss="modal" aria-label="Close">
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x">
                         <line x1="18" y1="6" x2="6" y2="18"></line>
